f7135445607405b9d0e5d174f24f0fb590a406f7
Jolvera2001 <143431900+Jolvera2001@users.noreply.github.com>
2024-02-01 21:12:40 -0600

    started making player component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createRoot } from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="canvas-container">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
   )
 }
 
---
+++ front-end/src/components/Player.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
+const PlayerComponent: React.FC = () => {
+    const [position, setPosition] = useState({x: 0, y: 0});
+
+    useEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+            switch (event.key) {
+                case 'w':
+                    setPosition((prev) => ({...prev, y: prev.y - 10}));
+                    break;
+                case 'a':
+                    setPosition((prev) => ({ ...prev, y: prev.x - 10 }));
+                    break;
+                case 's':
+                    setPosition((prev) => ({ ...prev, y: prev.y + 10 }));
+                    break;
+                case 'd':
+                    setPosition((prev) => ({ ...prev, y: prev.x + 10 }));
+                    break;
+            }
+        };
+
+        // attach listener when mounted
+        window.addEventListener('keydown', handleKeyDown);
+
+        // detach when not mounted
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []); 
+
+    return (
+        
+    )
+}
+
+export default PlayerComponent
