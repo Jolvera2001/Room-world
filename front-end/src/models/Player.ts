186759509f942c1f7ea21be76b06a6b6dba00de3
Jolvera2001 <143431900+Jolvera2001@users.noreply.github.com>
2024-02-01 20:43:24 -0600

    added player model

---
+++ front-end/src/models/Player.ts
@@ -0,0 +1,10 @@
+interface Player {
+    username: string;
+    interactions: number;
+    controllable: boolean;
+    xCoords: number;
+    yCoords: number;
+    zCoords: number;
+}
+
+export default Player;
