386946eb1e86fb1e1554ee4fd0a3296eb296c208
Jolvera2001 <143431900+Jolvera2001@users.noreply.github.com>
2024-02-03 01:31:36 -0600

    idk

--- front-end/src/debug/DCanvas.tsx
+++ front-end/src/debug/DCanvas.tsx
@@ -1,8 +1,20 @@
 // the purpose of this is to debug models and meshes on a more percievable plane
 // utilzing grids and such
-import React, { useState, useEffect } from 'react';
-import { Text } from '@react-three/drei'
+import React, { useState, useEffect, ReactNode } from 'react';
+import { Text, Grid } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber';
 
-const DCanvas: React.FC = () => {
-
+interface DCanvasProps {
+    children: ReactNode;
 }
+
+const DCanvas: React.FC<DCanvasProps> = ({ children }) => {
+    return (
+        <Canvas>
+            <Grid />
+            {children}
+        </Canvas>
+    )
+}
+
+export default DCanvas
