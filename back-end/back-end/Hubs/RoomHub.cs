3c761ea084c7287a943a349fa2c715bceb9b7fe1
Jolvera2001 <143431900+Jolvera2001@users.noreply.github.com>
2024-02-06 11:48:52 -0600

    Created Room Hub for player connection count

---
+++ back-end/back-end/Hubs/RoomHub.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace back_end.Hubs;
+
+public class RoomHub : Hub
+{
+    // place to store players in our room
+    // roomname : set of playerIDs
+    private static Dictionary<string, HashSet<string>> roomPlayerConnections = new Dictionary<string, HashSet<string>>();
+
+    public override async Task OnConnectedAsync()
+    {
+        await base.OnConnectedAsync();
+
+        string roomName = Context.GetHttpContext().Request.Query["roomName"];
+        if (!string.IsNullOrEmpty(roomName))
+        {
+            lock (roomPlayerConnections)
+            {
+                if (!roomPlayerConnections.ContainsKey(roomName))
+                {
+                    roomPlayerConnections[roomName] = new HashSet<string>();
+                }
+
+                roomPlayerConnections[roomName].Add(Context.ConnectionId);
+            }
+            
+            await Clients.Group(roomName).SendAsync("PlayerCountUpdated", roomPlayerConnections[roomName].Count);
+        }
+    }
+
+    public override async Task OnDisconnectedAsync(Exception exception)
+    {
+        await base.OnDisconnectedAsync(exception);
+
+        string roomName = Context.GetHttpContext().Request.Query["roomName"];
+        if (!string.IsNullOrEmpty(roomName))
+        {
+            lock (roomPlayerConnections)
+            {
+                if (roomPlayerConnections.ContainsKey(roomName))
+                {
+                    roomPlayerConnections[roomName].Remove(Context.ConnectionId);
+                }
+            } 
+            
+            await Clients.Group(roomName).SendAsync("PlayerCountUpdated", roomPlayerConnections[roomName].Count);
+        }
+    }
+}
