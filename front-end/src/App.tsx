a028eef26fd9687eb3459403ef1a4113637946a8
Jolvera2001 <143431900+Jolvera2001@users.noreply.github.com>
2024-02-06 12:15:01 -0600

    Testing for connecting to SignalR Hub

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,18 +1,9 @@
-import { Canvas } from '@react-three/fiber'
-import Player from './components/Player'
+import Room from './components/Room'
 
 function App() {
   return (
     <div id="canvas-container" className="w-full h-screen bg-black">
-      <Canvas>
-        <ambientLight intensity={0.25} />
-        <pointLight position={[0, 20, 10]} intensity={2} />
-        <directionalLight color="blue" position={[0, 0, 10]} />
-        {/* <DCanvas>
-          <Player />
-        </DCanvas> */}
-        <Player />
-      </Canvas>
+      <Room />
     </div>
   )
 }
--- front-end/src/components/Room.tsx
+++ front-end/src/components/Room.tsx
@@ -1,16 +1,44 @@
 import { HubConnectionBuilder } from "@microsoft/signalr";
+import { useEffect, useState } from "react";
+import { Text } from '@react-three/drei';
 import { Canvas } from "@react-three/fiber";
 import Player from "./Player";
 
-function Room() {
+const RoomComponent: React.FC = () => {
+    const [playerCount, setPlayerCount] = useState<number>(0);
+
+    useEffect(() => {
+        const connection = new HubConnectionBuilder()
+            .withUrl("/room")
+            .build();
+
+        connection.start()
+            .then(() => console.log("SignalR connection established"))
+            .catch(err => console.error("Error establishing SignalR connection:", err));
+
+        connection.on("PlayerCountUpdated", (count:number) => {
+            setPlayerCount(count);
+        })
+
+        return () => {
+            connection.stop()
+                .then(() => console.log("SignalR connection stopped"))
+                .catch(err => console.error("Error stopping signalR connection", err));
+        }; 
+    }, []);
+
+
     return (
         <div className="bg-white">
             <Canvas>
                 <ambientLight intensity={0.50} />
+                <Text position={[-1, -1, 0]} scale={0.2}>
+                    playerCount: {playerCount}
+                </Text>
                 <Player />
             </Canvas>
         </div>
     )
 }
 
-export default Room
+export default RoomComponent
