3aa5cba86f2d789c00b7b02136b98a8a02cfbd90
Jolvera2001 <143431900+Jolvera2001@users.noreply.github.com>
2024-02-08 14:16:08 -0600

    Started new tests

--- back-end/SignalR Server Test/UnitTest1.cs
+++ back-end/SignalR Server Test/UnitTest1.cs
@@ -3,8 +3,27 @@ namespace SignalR_Server_Test;
 public class RoomHubConnection
 {
     [Fact]
-    public void BasicConnection()
+    public async void BasicConnection()
     {
+        // creating connection to SignalR Hub
+        var connection = new HubConnectionBuilder()
+            .WithUrl("http://localhost:5117/room")
+            .Build();
+
+        await connection.StartAsync();
         
+        // checking connection
+        connection.State.Should().Be(HubConnectionState.Connected);
+    }
+
+    [Fact]
+    public async void ConnectionWithArgs()
+    {
+        // creating connection to SignalR Hub
+        var connection = new HubConnectionBuilder()
+            .WithUrl("http://localhost:5117/room")
+            .Build();
+
+        await connection.StartAsync();
     }
 }
