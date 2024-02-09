99e095505e7457d45b6e5bea2b5df2c86d702f1d
Jolvera2001 <jolvera6@stedwards.edu>
2024-02-08 22:02:44 -0600

    Updated tests and successful SignalR connections

--- back-end/SignalR Server Test/PlayerTests.cs
+++ back-end/SignalR Server Test/PlayerTests.cs
@@ -14,4 +14,9 @@ public class PlayerTests
         connection.State.Should().Be(HubConnectionState.Connected);
     }
 
+    public async Task UpdatePositon_SendsCoordsToPlayers()
+    {
+        
+    }
+
 }
